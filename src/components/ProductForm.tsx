4b8bcde440ba8714fa4ffda8dd11a807409c83dd
Wan Muzaffar <wanmuz86@gmail.com>
2025-12-04 16:03:03 +0800

    add form

--- src/components/ProductForm.tsx
+++ src/components/ProductForm.tsx
@@ -19,11 +19,30 @@ const ProductForm:React.FC<Props> = ({initial, onSubmit, submitLabel='Save'}) =>
     const [price, setPrice] = useState<number>(initial?.price ?? 0);
     const [image, setImage] = useState<string>(initial?.image ?? '');
     const [description, setDescription] = useState<string>(initial?.description ?? '');
-    // Error handling check form is filled or not
+    
+    //Loading... for the button
     const [busy, setBusy] = useState<boolean>(false);
 
   return (
-    <div>ProductForm</div>
+    <form>
+        <div>
+        <label htmlFor="title">Title</label>
+        <input type="text" value={title} onChange={(e)=> setTitle(e.target.value)} required/>
+        </div>
+        <div>
+            <label htmlFor="price">Price</label>
+            <input type="number" value={price} onChange={(e)=> setPrice(Number(e.target.value))} required/>
+        </div>
+        <div>
+            <label htmlFor="image">Image URL</label>
+            <input type="text" value={image} onChange={(e)=>setImage(e.target.value)} />
+        </div>
+        <label htmlFor="description">
+            Description
+            <textarea value={description} rows={4} onChange={(e)=>setDescription(e.target.value)}></textarea>
+        </label>
+        <button type="submit" disabled={busy}>{busy ? 'Saving...' : submitLabel }</button>
+    </form>
   )
 }
 
