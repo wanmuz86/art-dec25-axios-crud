87caeced927c73fe56e35fe303d96c3c5f34e07e
Wan Muzaffar <wanmuz86@gmail.com>
2025-12-04 14:57:33 +0800

    add logic for fetching data

--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,7 +1,34 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { Product } from '../models/Product';
+import { fetchProducts } from '../services/ProductService';
 
 const ProductList = () => {
+    const [items, setItems] = useState<Product[]>([]);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [error,setError] = useState<string | null>(null);
+
+    // retrive data from API
+
+    const loadData = async () => {
+        setLoading(true)
+        try {
+            const data = await fetchProducts()
+            setItems(data);
+        }
+        catch (error){
+            setError('Error fetch data')
+        }
+        finally {
+            setLoading(false);
+
+        }
+    }
+    // when the page is loaded / retrieve the data from API
+    
+    useEffect(()=>{
+        loadData();
+    },[]);
+
   return (
     <div>ProductList</div>
   )
