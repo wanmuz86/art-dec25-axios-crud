afdc454182f838c75fc5c0af49bc69da759410a9
Wan Muzaffar <wanmuz86@gmail.com>
2025-12-04 14:48:18 +0800

    update the route

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
 
-
-
-const Layout = () => {
+const Layout:React.FC = () => {
   return (
-    <div>Layout</div>
+    <div style={{maxWidth:960, margin:'24px auto', padding:16}}>
+        <h1>Products</h1>
+        <nav style={{display:'flex', gap:12, marginBottom:16}}>
+            <NavLink to="/">List</NavLink>
+            <NavLink to="/products/new">Create</NavLink>
+        </nav>
+        <Outlet/>
+    </div>
   )
 }
 
