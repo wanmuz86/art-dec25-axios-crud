1816e7da1e20ba49463da706516f1bcb9005d3fd
Wan Muzaffar <wanmuz86@gmail.com>
2025-12-04 16:12:47 +0800

    complete product create

--- src/pages/ProductCreate.tsx
+++ src/pages/ProductCreate.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import ProductForm from '../components/ProductForm'
+import { createProduct } from '../services/ProductService'
+import { useNavigate } from 'react-router-dom'
 
+const ProductCreate: React.FC = () => {
+    // Callback function that will be called from the 'Shared' Form
+    // typeof createProduct> = > parameters from the method createProduct in our service
+    // ProductInput
 
-
-const ProductCreate = () => {
+    const navigate = useNavigate()
+    const handleSubmit = async (values: Parameters<typeof createProduct>[0]) => {
+        try {
+            const created = await createProduct(values);
+            console.log(created); //check the data returned from the API
+            navigate(`/products/${created.id}`);
+        } catch (e:any){
+            alert(e?.message || 'created failed')
+        }
+    }
   return (
-    <div>ProductCreate</div>
+    <div>
+        <h2>Create Product</h2>
+        <ProductForm onSubmit={handleSubmit} submitLabel='Create'/>
+    </div>
   )
 }
 
